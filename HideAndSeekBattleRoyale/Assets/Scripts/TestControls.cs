96f1bbd2343e913cb4b4a2fb2645a19479287773
Milly <milabits@gmail.com>
2018-11-08 13:30:23 +0100

    Basic FOV system

---
+++ HideAndSeekBattleRoyale/Assets/Scripts/TestControls.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestControls : MonoBehaviour {
+    public float speed;
+
+    // Update is called once per frame
+    void Update() {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime);
+        transform.Translate(move);
+    }
+}
