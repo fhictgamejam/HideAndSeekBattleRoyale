bb2a68f30ea7b4a8436dba9e9305da78c0c50663
Milly <milabits@gmail.com>
2018-11-08 15:30:49 +0100

    Added lights

---
+++ HideAndSeekBattleRoyale/Assets/Scripts/Field of View/Light.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Light : MonoBehaviour {
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("Enter");
+        if (other.gameObject.GetComponent<Visibility>() != null)
+            other.gameObject.GetComponent<Visibility>().IsVisible = true;
+    }
+
+    private void OnTriggerExit(Collider other) {
+        Debug.Log("Exit");
+        if (other.gameObject.GetComponent<Visibility>() != null)
+            other.gameObject.GetComponent<Visibility>().IsVisible = false;
+    }
+
+    private void Start() {
+        DestroyImmediate(GetComponent<MeshCollider>());
+        MeshCollider coll = gameObject.AddComponent<MeshCollider>();
+        coll.sharedMesh = GetComponent<MeshFilter>().mesh;
+        coll.convex = true;
+        coll.isTrigger = true;
+    }
+}
