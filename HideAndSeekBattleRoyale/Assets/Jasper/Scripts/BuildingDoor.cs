1705f03a0a7a32377e580c610b95278f69510c3e
Teun <teun.vandenbroek@student.fontys.nl>
2018-11-08 14:45:59 +0100

    test push
    test push

---
+++ HideAndSeekBattleRoyale/Assets/Jasper/Scripts/BuildingDoor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingDoor : MonoBehaviour {
+
+	public GameObject roof;
+
+
+	private void OnTriggerEnter(Collider other)
+	{
+		roof.GetComponent<Renderer>().enabled = false;
+	}
+	private void OnTriggerExit(Collider other)
+	{
+		roof.GetComponent<Renderer>().enabled = true;
+	}
+}
