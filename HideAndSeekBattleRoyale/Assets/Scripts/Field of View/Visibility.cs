3faa4926851032c8cff1f48702eb560317cd1759
Milly <milabits@gmail.com>
2018-11-08 19:54:29 +0100

    Player visually changes upon entering/leaving light

--- HideAndSeekBattleRoyale/Assets/Scripts/Field of View/Visibility.cs
+++ HideAndSeekBattleRoyale/Assets/Scripts/Field of View/Visibility.cs
@@ -2,4 +2,24 @@
 
 public class Visibility : MonoBehaviour {
     public bool IsVisible;
+
+    private MeshRenderer meshRenderer;
+
+    public Material visibleMat;
+    public Material invisibleMat;
+
+    private void Start() {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void Update() {
+        if (IsVisible && meshRenderer.sharedMaterial != visibleMat) {
+            meshRenderer.sharedMaterial = visibleMat;
+            return;
+        }
+
+        if (!IsVisible && meshRenderer.sharedMaterial != invisibleMat) {
+            meshRenderer.sharedMaterial = invisibleMat;
+        }
+    }
 }
