6480701c81e2ad2b1a7ab20684b191208d6443b0
Jasper de Kroon <jasper.dekroon@student.fontys.nl>
2018-11-09 13:24:30 +0100

    scoreboard, scores hoeven alleen nog aan de speler gegeven te worden. 2 punten worden per 2/3 seconden toegevoegd

--- HideAndSeekBattleRoyale/Assets/Scripts/PlayerScore.cs
+++ HideAndSeekBattleRoyale/Assets/Scripts/PlayerScore.cs
@@ -1,15 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerScore : MonoBehaviour
 {
-	public int hitOppReward = 50;
+	public int hitOpponentReward = 50;
 	public int getHitLoss = 20;
 	private int score;
 	private float timer;
 	int timeDif;
-	int timeSec;
+
+	public GameObject parent;
+	public Text scorePrefab;
+
+	float downPlacement = 0;
+
+
+	private List<Text> scores = new List<Text>();
+	private List<int> plScores = new List<int>();
+	private List<int> oldPlScores = new List<int>();
 	// Use this for initialization
 	void Start()
 	{
@@ -20,22 +30,56 @@ public class PlayerScore : MonoBehaviour
 	void Update()
 	{
 		ScoreTime();
-
 	}
+	public void AddPlayerScore(List<int> gScores)
+	{
+		plScores.Clear();
+		oldPlScores = gScores;
+		if (gScores == null)
+		{
+			gScores = new List<int>();
+		}
+		else
+		{
+			plScores = gScores;
+		}
+		gScores.Add(this.score);
+		InstantiateScoreFields();
+	}
+	private void InstantiateScoreFields()
+	{
+		plScores.Sort((a, b) => -1 * a.CompareTo(b));
+		foreach (Text sText in scores)
+		{
+			Destroy(sText.gameObject);
+		}
+		scores.Clear();
+		foreach (var s in plScores)
+		{
+			Text plScore = Instantiate(scorePrefab, parent.transform);
+			plScore.transform.Translate(new Vector3(0, -downPlacement));
+			downPlacement += plScore.rectTransform.rect.height;
+			plScore.text = s.ToString();
+			scores.Add(plScore);
+		}
+		downPlacement = 0;
+	}
+
 	private void ScoreTime()
 	{
 		timer += Time.deltaTime;
 		timeDif = (int)timer % 60;
-		timeSec = (int)timer % 60;
 		if (timeDif >= 2)
 		{
-			score++;
-			timeDif = 0;
+			timer = 0;
+			score += 2;
 		}
+		AddPlayerScore(oldPlScores);
 	}
 	public void HitOpponent()
 	{
-		score += hitOppReward;
+		score += hitOpponentReward;
+		AddPlayerScore(oldPlScores);
 	}
 	public void GetHit()
 	{
@@ -44,5 +88,11 @@ public class PlayerScore : MonoBehaviour
 		{
 			score = 0;
 		}
+		AddPlayerScore(oldPlScores);
+	}
+
+	public int GetScore()
+	{
+		return this.score;
 	}
 }
